1a86d41613a140f6a5a0fc732307664e9477dcb3
Ali Sancaktar <asancaktar01@gmail.com>
2023-07-04 10:32:16 +0300

    Student1 class created

---
+++ src/Student/Student1.java
@@ -0,0 +1,10 @@
+package Student;
+
+public class Student1 {
+    public void myMethod() {
+        for (int i = 0; i <10 ; i++) {
+            System.out.println("Hello World");
+        }
+
+    }
+}
