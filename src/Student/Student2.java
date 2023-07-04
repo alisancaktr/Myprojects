08f670d2f3f216e746632e1e9f8924ff94c6cc4b
Ali Sancaktar <asancaktar01@gmail.com>
2023-07-04 13:33:11 +0300

    sout changed

--- src/Student/Student2.java
+++ src/Student/Student2.java
@@ -1,9 +1,10 @@
 package Student;
 
 public class Student2 {
+
     public void myMethod2(){
         for (int i = 0; i <5 ; i++) {
-            System.out.println("String");
+            System.out.println("Str21");
         }
     }
 }
